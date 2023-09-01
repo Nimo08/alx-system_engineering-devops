4626fb850235c016c0afa9244368dbce7d0d12d0
Nimo08 <nimomohamed800@gmail.com>
2023-09-01 19:46:04 +0300

    Task 11

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+/**
+ * main -entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	pid_t child;
+
+	for (i = 0; i < 5; i++)
+	{
+		child = fork();
+		if (child > 0)
+		{
+			sleep(5);
+			printf("Zombie process created, PID: %d\n", child);
+		}
+		else
+		{
+			exit(0);
+		}
+	}
+	return (0);
+}
+/**
+ * infinite_while - runs an infinite while loop
+ * Return: 0
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
