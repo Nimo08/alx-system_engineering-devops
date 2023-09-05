ed1486e28081384ed21f62d55b55a85ef2538e69
Nimo08 <nimomohamed800@gmail.com>
2023-09-05 17:02:55 +0300

    Task 8

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,10 +1,5 @@
 #!/usr/bin/env ruby
 log = ARGV[0]
-
-sender_match = log.match(/\[from:(.*?)\]/)
-receiver_match = log.match(/\[to:(.*?)\]/)
-flags_match = log.match(/\[flags:(.*?)\]/)
-if sender_match && receiver_match && flags_match
-  res = "#{sender_match[1]}, #{receiver_match[1]}, #{flags_match[1]}"
-  puts res
-end
+match = log.scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/)
+res = match.join(',')
+puts res
