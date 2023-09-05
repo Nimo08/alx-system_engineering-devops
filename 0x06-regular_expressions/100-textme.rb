1d77cb50734b3fbce349c187558e819bf57e711b
Nimo08 <nimomohamed800@gmail.com>
2023-09-05 15:49:14 +0300

    Task 8

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+log = ARGV[0]
+
+sender_match = log.match(/\[from:(.*?)\]/)
+receiver_match = log.match(/\[to:(.*?)\]/)
+flags_match = log.match(/\[flags:(.*?)\]/)
+
+print sender_match[1] + ", " + receiver_match[1] + ", " + flags_match[1] + "\n"
