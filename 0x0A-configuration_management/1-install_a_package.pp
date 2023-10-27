84a79a188fd6c02a8e07e7308d72a824c9dfa5d5
Nimo08 <nimomohamed800@gmail.com>
2023-10-27 12:26:25 +0300

    Task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 # install flask from pip3
 package { 'flask':
-  ensure   => 'installed',
-  provider => 'pip3',
+  ensure          => 'installed',
+  provider        => 'pip3',
+  install_options => ['--prefix=/usr'],
 }
