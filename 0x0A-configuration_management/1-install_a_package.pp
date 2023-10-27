199557f69645090be0ff27a20edab95078557d81
Nimo08 <nimomohamed800@gmail.com>
2023-10-27 12:45:36 +0300

    reverted to pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,5 @@
 # install flask from pip3
-
-package { 'python3-pip':
-  ensure => 'installed',
-}
-
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip',
-  require  => Package['python3-pip'],
+  provider => 'pip3',
 }
