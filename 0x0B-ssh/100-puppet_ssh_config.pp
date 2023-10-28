55a4a170e17fc2bf418c5c5da421929b883a7aba
Nimo08 <nimomohamed800@gmail.com>
2023-10-28 11:29:56 +0300

    Task 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # make changes to our configuration file
 augeas { 'ssh_config':
-  context => '/files/home/*/ssh/ssh_config',
+  context => '/files/etc/ssh/ssh_config',
   changes => [
     'set PasswordAuthentication no',
     'set IdentityFile ~/.ssh/school',
