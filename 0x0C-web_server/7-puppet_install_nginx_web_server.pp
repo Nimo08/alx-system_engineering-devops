6699fef34ec066e5331638b08d0582995d8ff4dd
Nimo08 <nimomohamed800@gmail.com>
2023-10-31 18:15:46 +0300

    Edited configuration

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,21 +1,28 @@
 # Install and configure an Nginx server using Puppet
-class { 'nginx':
-  listen_port => 80,
-}
+include nginx
 file { '/etc/nginx/sites-available/redirect_me':
   ensure => file,
 }
 nginx::resource::server { 'default':
+  ensure             => present,
+  spdy               => 'off',
+  http2              => 'off',
+  proxy_read_timeout => '60s',
+  proxy_send_timeout => '60s',
+  proxy_set_header   => [],
+  proxy_hide_header  => [],
+  proxy_pass_header  => [],
+  owner              => 'www-data',
+  group              => 'www-data',
+  mode               => '0644',
+}
+nginx::resource::location { 'index.html':
   ensure   => present,
-  location => [
-    {
-      location => '/',
-      content  => 'Hello World!',
-    },
-    {
-      location => '^/redirect_me$',
-      rewrite  => 'https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-    },
-  ],
+  location => '/',
+  content  => 'Hello World!',
+}
+nginx::resource::location { 'redirect_me':
+  location => '^/redirect_me$',
+  rewrite  => 'https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
   require  => File['/etc/nginx/sites-available/redirect_me'],
 }
