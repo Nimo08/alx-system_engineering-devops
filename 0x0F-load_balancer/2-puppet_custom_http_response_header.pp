35c3045572967bcd3e2d0c9b9f47e52a453ce1b9
Nimo08 <nimomohamed800@gmail.com>
2023-11-06 14:52:47 +0300

    Restructured puppet manifest

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,19 +1,7 @@
 # Automate the task of creating a custom HTTP header response, but with Puppet
 
-$server_hostname = $facts['networking']['fqdn']
-
-package { 'nginx':
-  ensure => 'installed',
-}
-
-file_line { 'add custom http header':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'location / {',
-  line   => 'add_header X-Served-By "$server_hostname";',
-}
-
-service { 'nginx':
-  ensure  => 'running',
-  require => Package[nginx],
+exec { 'add custom http header':
+  command => "echo 'add_header X-Served-By \{"$server_hostname\}";' >> /etc/nginx/sites-available/default",
+  require => exec { 'install nginx package' },
+  notify  => Service['nginx'],
 }
