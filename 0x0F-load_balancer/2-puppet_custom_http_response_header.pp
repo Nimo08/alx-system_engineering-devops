8842c31c8e56de5e27a0ae50455d23f4e0987220
Nimo08 <nimomohamed800@gmail.com>
2023-11-06 14:07:13 +0300

    Directly state server name

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -8,7 +8,7 @@ file_line { 'install':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'location / {',
-  line   => 'add_header X-Served-By $server_hostname;',
+  line   => 'add_header X-Served-By 405012-web-01;',
 }
 
 service { 'nginx':
