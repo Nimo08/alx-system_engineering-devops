5ee2071ce62205b9ef5c8ee271a6b3fe85ca7e02
Nimo08 <nimomohamed800@gmail.com>
2023-11-06 19:18:04 +0300

    hostname and sudo

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,7 +1,7 @@
 # Automate the task of creating a custom HTTP header response, but with Puppet
 exec { 'command':
   command  => 'apt-get -y update; apt-get -y install nginx;
-  sudo sed -i "51i add_header X-Served-By $hostname;" /etc/nginx/sites-available/default;
+  sed -i "51i add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
   service nginx restart',
   provider => shell,
 }
