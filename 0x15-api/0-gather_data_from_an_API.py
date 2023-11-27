5ca3f30075bc4c8fae79f91e4dccf1bec8633adc
Nimo08 <nimomohamed800@gmail.com>
2023-11-27 12:06:13 +0300

    Attempting tasks 0 & 1

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+Use REST API for a given employee ID to return
+information about his/her TODO list progress
+"""
+
+
+import requests
+import sys
+
+
+def todo_list(employee_id):
+    """
+    Returns information about an employee's TODO list
+    """
+    url_api = f'https://jsonplaceholder.typicode.com/'\
+              f'todos?userId={employee_id}'
+    try:
+        # make GET request to todo list API endpoint
+        response = requests.get(url_api)
+        response.raise_for_status()
+        todo = response.json()
+        # make separate GET request to user endpoint for user info
+        user_response = requests.get(f'https://jsonplaceholder.typicode.com/'
+                                     f'users/{employee_id}')
+        user_response.raise_for_status()
+        user_info = user_response.json()
+        employee_name = user_info['name']
+        completed_tasks = []
+        for task in todo:
+            if task['completed']:
+                completed_tasks.append(task)
+        total_tasks = len(todo)
+        completed_task_num = len(completed_tasks)
+        print(f"Employee {employee_name} is done with tasks"
+              f"({str(completed_task_num)}/{str(total_tasks)}):")
+        for task in completed_tasks:
+            print(f"    {task['title']}")
+    except requests.exceptions.RequestException as e:
+        print(e)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.exit(1)
+    try:
+        employee_id = int(sys.argv[1])
+        todo_list(employee_id)
+    except ValueError as e:
+        print(e)
+        sys.exit(1)
---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""
+Use REST API for a given employee ID to return
+information about his/her TODO list progress
+Export data to CSV format
+"""
+
+
+import csv
+import requests
+import sys
+
+
+def todo_list(employee_id):
+    """
+    Returns information about an employee's TODO list
+    """
+    url_api = f'https://jsonplaceholder.typicode.com/'\
+              f'todos?userId={employee_id}'
+    try:
+        # make GET request to todo list API endpoint
+        response = requests.get(url_api)
+        response.raise_for_status()
+        todo = response.json()
+        # make separate GET request to user endpoint for user info
+        user_response = requests.get(f'https://jsonplaceholder.typicode.com/'
+                                     f'users/{employee_id}')
+        user_response.raise_for_status()
+        user_info = user_response.json()
+        user_id = user_info['id']
+        username = user_info['name']
+        completed_tasks = []
+        for task in todo:
+            if task['completed']:
+                completed_tasks.append(task)
+        return {
+                'user_id': user_id,
+                'username': username,
+                'completed_tasks': completed_tasks
+                }
+    except requests.exceptions.RequestException as e:
+        print(e)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.exit(1)
+    try:
+        employee_id = int(sys.argv[1])
+        todo_list_info = todo_list(employee_id)
+        with open(f'{employee_id}.csv', 'w', newline='') as csvfile:
+            csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+            csv_writer.writerow(["USER_ID", "USERNAME",
+                                "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+            for task in todo_list_info['completed_tasks']:
+                csv_writer.writerow([
+                        str(todo_list_info['user_id']),
+                        todo_list_info['username'],
+                        str(task['completed']),
+                        task['title']
+                    ])
+    except ValueError as e:
+        print(e)
+        sys.exit(1)
