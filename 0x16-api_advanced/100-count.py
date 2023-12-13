9a014dac90999b7ece903db4d69715c20463ceb2
Nimo08 <nimomohamed800@gmail.com>
2023-12-13 19:07:53 +0300

    Corrected naming error

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -12,36 +12,42 @@ import json
 import requests
 
 
-def count_words(subreddit, word_list, after='', res={}):
+def count_words(subreddit, word_list, after='', res=None):
     """
      Prints a sorted count of given keywords (case-insensitive,
      delimited by spaces.
      If no posts match or the subreddit is invalid, print nothing.
     """
+    if res is None:
+        res = {}
+
     if not res:
         for word in word_list:
             if word.lower() not in res:
                 res[word.lower()] = 0
     if after is None:
-        res = sorted(res.items(), key=lambda q: (-q[1], q[0]))
-        for word in res:
-            if word[1]:
-                print(f'{word[0]}, {word[1]}')
+        sorted_res = sorted(res.items(), key=lambda q: (-q[1], q[0]))
+        for word, count in sorted_res:
+            if count > 0:
+                print(f'{word}, {count}')
         return None
+
     user_agent = 'RedditBot'
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
     headers = {'User-Agent': user_agent}
     params = {'limit': 100, 'after': after}
+
     try:
+        response = requests.get(url, headers=headers, params=params)
         if response.status_code == 200:
             posts = response.json()['data']['children']
             next_after = response.json()['data']['after']
 
-        for post in posts:
-            title = post['data']['title']
-            lower_word = [word.lower() for word in title.split(' ')]
-            for word in res.keys():
-                res[word] += lower_word.count(word)
+            for post in posts:
+                title = post['data']['title']
+                lower_word = [word.lower() for word in title.split(' ')]
+                for word in res.keys():
+                    res[word] += lower_word.count(word)
     except Exception:
         return None
-    count_words(subreddit, word_list, after, res)
+    count_words(subreddit, word_list, after=next_after, res=res)
