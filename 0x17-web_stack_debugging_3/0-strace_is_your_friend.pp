7f74ac6e0ed3e40162dce5c02991726c00ba9353
Nimo08 <nimomohamed800@gmail.com>
2023-12-14 13:13:51 +0300

    Comment

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,4 @@
+# Using strace, find out why Apache is returning a 500 error.
 file {'/var/www/html/wp-includes/class-wp-locale.phpp':
   ensure => file,
   source => '/var/www/html/wp-includes/class-wp-locale.php',
