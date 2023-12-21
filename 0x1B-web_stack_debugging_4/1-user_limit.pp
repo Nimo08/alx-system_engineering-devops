e07cb77d60878500173736615b1b11dbf25fc50c
Nimo08 <nimomohamed800@gmail.com>
2023-12-21 08:44:11 +0300

    Tasks

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,17 +1,11 @@
-# Fix stack to get failed requests to 0
 # Change the OS configuration to login with the holberton user
 
-exec {'replace_u-limit':
-  path    => ['/bin', '/usr/bin'],
-  command => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
-  before  => Exec['restart_nginx'],
+exec { 'hard_file_limit':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
 
-exec {'restart_nginx':
-  path    => ['/bin', '/usr/bin'],
-  command => 'sudo service nginx restart',
-}
-
-if $ulimit_value != '15' {
-  Exec['replace_u-limit'] -> Exec['restart_nginx']
+exec { 'soft_file_limit':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
